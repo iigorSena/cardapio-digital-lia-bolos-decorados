604d7b3b2cdbac6ba909dead52a5aaf2e0856b3d
iigorSena <igor_senna@outlook.con.br>
2025-06-04 16:01:59 -0300

    Inserido título do checkout

--- js/lista-produtos.js
+++ js/lista-produtos.js
@@ -82,181 +82,182 @@ const cardapioData = {
     }
   ],
   'doces': [
-    {
-      imagem: 'img/img-doces-1.jpg',
-      descricao: 'Porta retrato de chocolate',
-      preco: 'R$ 8,50 und',
-      und: 'Unidade',
-      quant: 1,
-      valor: 8.5
-    },
-    {
-      imagem: 'img/img-doces-2.jpg',
-      descricao: 'Maçãs 2D',
-      preco: 'R$ 8,00 und',
-      und: 'Unidade',
-      quant: 1,
-      valor: 8
-    },
-    {
-      imagem: 'img/img-doces-3.jpg',
-      descricao: 'Pirulito',
-      preco: 'R$ 8,50 und',
-      und: 'Unidade',
-      quant: 1,
-      valor: 8.50
-    },
-    {
-      imagem: 'img/img-doces-4.jpg',
-      descricao: 'Pirulito 3D',
-      preco: 'R$ 10,00 und',
-      und: 'Unidade',
-      quant: 1,
-      valor: 10
-    },
-    {
-      imagem: 'img/img-doces-5.jpg',
-      descricao: 'Cupcake 2D (Personalizado/Com Saiote)',
-      preco: 'R$ 8,50 und',
-      und: 'Unidade',
-      quant: 1,
-      valor: 8.50
-    },
-    {
-      imagem: 'img/img-doces-5.jpg',
-      descricao: 'Cupcake 2D (Personalizado/Com Saiote) com recheio',
-      preco: 'R$ 10,00  und',
-      und: 'Unidade',
-      quant: 1,
-      valor: 10
-    },
-    {
-      imagem: 'img/img-doces-6.jpg',
-      descricao: 'Cupcake chantilly',
-      preco: 'R$ 4,50 und',
-      und: 'Unidade',
-      quant: 1,
-      valor: 4.50
-    },
-    {
-      imagem: 'img/img-doces-6.jpg',
-      descricao: 'Cupcake chantilly',
-      preco: 'R$ 6,00 und',
-      und: 'Unidade',
-      quant: 1,
-      valor: 6
-    },
-    {
-      imagem: 'img/img-doces-7.jpg',
-      descricao: 'Cupcake 3D',
-      preco: 'R$ 12,00 und',
-      und: 'Unidade',
-      quant: 1,
-      valor: 12
-    },
-    {
-      imagem: 'img/img-doces-8.jpg',
-      descricao: 'Picolé 2D',
-      preco: 'R$ 9,50 und',
-      und: 'Unidade',
-      quant: 1,
-      valor: 9.50
-    },
-    {
-      imagem: 'img/img-doces-9.jpg',
-      descricao: 'Picolé 3D',
-      preco: 'R$ 16,00 und',
-      und: 'Unidade',
-      quant: 1,
-      valor: 16
-    },
-    {
-      imagem: 'img/img-doces-10.jpg',
-      descricao: 'Trufa 2D',
-      preco: 'R$ 9,00 und',
-      und: 'Unidade',
-      quant: 1,
-      valor: 9
-    },
-    {
-      imagem: 'img/img-doces-11.jpg',
-      descricao: 'Trufa Personagem',
-      preco: 'R$ 10,00 und',
-      und: 'Unidade',
-      quant: 1,
-      valor: 10
-    },
-    {
-      imagem: 'img/img-doces-12.jpg',
-      descricao: 'Trufa 3D com personagem',
-      preco: 'R$ 15,00 und',
-      und: 'Unidade',
-      quant: 1,
-      valor: 15
-    },
-    {
-      imagem: 'img/img-doces-13.jpg',
-      descricao: 'Trufa 3D dupla',
-      preco: 'R$ 18,00 und',
-      und: 'Unidade',
-      quant: 1,
-      valor: 18
-    },
-    {
-      imagem: 'img/img-doces-14.jpg',
-      descricao: 'Bolo no Palito 2D',
-      preco: 'R$ 13,00 und',
-      und: 'Unidade',
-      quant: 1,
-      valor: 13
-    },
-    {
-      imagem: 'img/img-doces-15.jpg',
-      descricao: 'Cone com personagem',
-      preco: 'R$ 12,00 und',
-      und: 'Unidade',
-      quant: 1,
-      valor: 12
-    },
-    {
-      imagem: 'img/img-doces-16.jpg',
-      descricao: 'Bolo 3D (Casinha / Poça de lama / Cama / Personagens sentados)',
-      preco: 'R$ 20,00 und',
-      und: 'Unidade',
-      quant: 1,
-      valor: 20
-    },
-    {
-      imagem: 'img/img-doces-17.jpg',
-      descricao: 'Doces modelado de leite ninho',
-      preco: 'R$ 7,00 und',
-      und: 'Unidade',
-      quant: 1,
-      valor: 7
-    },
-    {
-      imagem: 'img/img-doces-18.jpg',
-      descricao: 'Modelados leite ninho',
-      preco: 'R$ 250,00 (cento)',
-      und: 'Unidade',
-      quant: 1,
-      valor: 250
-    },
-    {
-      imagem: 'img/img-doces-19.jpg',
-      descricao: 'Doces personalizados',
-      preco: 'R$ 220,00 (cento)',
-      und: 'Unidade',
-      quant: 1,
-      valor: 220
-    },
-    {
-      imagem: 'img/img-doces-20.jpg',
-      descricao: 'Trufas personalizadas',
-      preco: 'R$ 200,00 (cento)',
-      und: 'Unidade',
-      quant: 1,
-      valor: 200
-    }
-  ]
+  {
+    imagem: 'img/img-doces-1.jpg',
+    descricao: 'Porta retrato de chocolate',
+    preco: 'R$ 8,50 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 8.5
+  },
+  {
+    imagem: 'img/img-doces-2.jpg',
+    descricao: 'Maçãs 2D',
+    preco: 'R$ 8,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 8
+  },
+  {
+    imagem: 'img/img-doces-3.jpg',
+    descricao: 'Pirulito',
+    preco: 'R$ 8,50 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 8.50
+  },
+  {
+    imagem: 'img/img-doces-4.jpg',
+    descricao: 'Pirulito 3D',
+    preco: 'R$ 10,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 10
+  },
+  {
+    imagem: 'img/img-doces-5.jpg',
+    descricao: 'Cupcake 2D (Personalizado/Com Saiote)',
+    preco: 'R$ 8,50 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 8.50
+  },
+  {
+    imagem: 'img/img-doces-5.jpg',
+    descricao: 'Cupcake 2D (Personalizado/Com Saiote) com recheio',
+    preco: 'R$ 10,00  und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 10
+  },
+  {
+    imagem: 'img/img-doces-6.jpg',
+    descricao: 'Cupcake chantilly',
+    preco: 'R$ 4,50 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 4.50
+  },
+  {
+    imagem: 'img/img-doces-6.jpg',
+    descricao: 'Cupcake chantilly',
+    preco: 'R$ 6,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 6
+  },
+  {
+    imagem: 'img/img-doces-7.jpg',
+    descricao: 'Cupcake 3D',
+    preco: 'R$ 12,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 12
+  },
+  {
+    imagem: 'img/img-doces-8.jpg',
+    descricao: 'Picolé 2D',
+    preco: 'R$ 9,50 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 9.50
+  },
+  {
+    imagem: 'img/img-doces-9.jpg',
+    descricao: 'Picolé 3D',
+    preco: 'R$ 16,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 16
+  },
+  {
+    imagem: 'img/img-doces-10.jpg',
+    descricao: 'Trufa 2D',
+    preco: 'R$ 9,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 9
+  },
+  {
+    imagem: 'img/img-doces-11.jpg',
+    descricao: 'Trufa Personagem',
+    preco: 'R$ 10,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 10
+  },
+  {
+    imagem: 'img/img-doces-12.jpg',
+    descricao: 'Trufa 3D com personagem',
+    preco: 'R$ 15,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 15
+  },
+  {
+    imagem: 'img/img-doces-13.jpg',
+    descricao: 'Trufa 3D dupla',
+    preco: 'R$ 18,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 18
+  },
+  {
+    imagem: 'img/img-doces-14.jpg',
+    descricao: 'Bolo no Palito 2D',
+    preco: 'R$ 13,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 13
+  },
+  {
+    imagem: 'img/img-doces-15.jpg',
+    descricao: 'Cone com personagem',
+    preco: 'R$ 12,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 12
+  },
+  {
+    imagem: 'img/img-doces-16.jpg',
+    descricao: 'Bolo 3D (Casinha / Poça de lama / Cama / Personagens sentados)',
+    preco: 'R$ 20,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 20
+  },
+  {
+    imagem: 'img/img-doces-17.jpg',
+    descricao: 'Doces modelado de leite ninho',
+    preco: 'R$ 7,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 7
+  },
+  {
+    imagem: 'img/img-doces-18.jpg',
+    descricao: 'Modelados leite ninho',
+    preco: 'R$ 250,00 (cento)',
+    und: 'Cento',
+    quant: 1,
+    valor: 250
+  },
+  {
+    imagem: 'img/img-doces-19.jpg',
+    descricao: 'Doces personalizados',
+    preco: 'R$ 220,00 (cento)',
+    und: 'Cento',
+    quant: 1,
+    valor: 220
+  },
+  {
+    imagem: 'img/img-doces-20.jpg',
+    descricao: 'Trufas personalizadas',
+    preco: 'R$ 200,00 (cento)',
+    und: 'Cento',
+    quant: 1,
+    valor: 200
+  }
+]
+
 };
