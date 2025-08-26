5c541e42ca8055fb8947fd1809b13d835f2a88cd
iigorSena <igor_senna@outlook.con.br>
2025-08-26 14:16:45 -0300

    segundo commit

---
+++ js/lista-produtos.js
@@ -0,0 +1,425 @@
+const cardapioData = {
+  'bolo-decorado': [
+    {
+      imagem: 'img/pasta-americana.jpeg',
+      descricao: 'Pasta americana',
+      massa: 'Massa de Margarina',
+      preco_kg: 'R$ 110,00 kg',
+      quant: 1.5,
+      valor: 110
+    },
+    {
+      imagem: 'img/Chantilly.jpeg',
+      descricao: 'Chantilly',
+      massa: 'Massa de Margarina',
+      preco_kg: 'R$ 95,00 kg',
+      quant: 1.5,
+      valor: 95
+    },
+    {
+      imagem: 'img/Naked-cake.jpeg',
+      descricao: 'Naked cake',
+      massa: 'Massa de Margarina',
+      preco_kg: 'R$ 90,00 kg',
+      quant: 1.5,
+      valor: 90
+    },
+    {
+      imagem: 'img/Naked-cake-frutas.jpg',
+      descricao: 'Naked cake com frutas',
+      massa: 'Massa de Margarina',
+      preco_kg: 'R$ 95,00 kg',
+      quant: 1.5,
+      valor: 95
+    },
+    {
+      imagem: 'img/Scrapcake.jpeg',
+      descricao: 'Scrapcake',
+      massa: 'Massa de Margarina',
+      preco_kg: 'R$ 95,00 kg',
+      quant: 1.5,
+      valor: 95
+    },
+    {
+      imagem: 'img/pasta-americana-man.jpg',
+      descricao: 'Pasta americana',
+      massa: 'Massa de Manteiga',
+      preco_kg: 'R$ 125,00 kg',
+      quant: 1.5,
+      valor: 125
+    },
+    {
+      imagem: 'img/Chantilly-man.jpeg',
+      descricao: 'Chantilly',
+      massa: 'Massa de Manteiga',
+      preco_kg: 'R$ 105,00 kg',
+      quant: 1.5,
+      valor: 105
+    },
+    {
+      imagem: 'img/Naked-cake.jpeg',
+      descricao: 'Naked Cake',
+      massa: 'Massa de Manteiga',
+      preco_kg: 'R$ 100,00 kg',
+      quant: 1.5,
+      valor: 100
+    },
+    {
+      imagem: 'img/Naked-cake-man.jpeg',
+      descricao: 'Naked Cake com frutas',
+      massa: 'Massa de Manteiga',
+      preco_kg: 'R$ 105,00 kg',
+      quant: 1.5,
+      valor: 105
+    },
+    {
+      imagem: 'img/Scrapcake-man.jpeg',
+      descricao: 'Scrapcake ',
+      massa: 'Massa de Manteiga',
+      preco_kg: 'R$ 105,00 kg',
+      quant: 1.5,
+      valor: 105
+    }
+  ],
+  'doces': [
+  {
+    imagem: 'img/doces/Porta-retrato.jpeg',
+    descricao: 'Porta retrato de chocolate',
+    preco: 'R$ 8,50 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 8.5
+  },
+  {
+    imagem: 'img/doces/Maçã-2d.jpeg',
+    descricao: 'Maçãs 2D',
+    preco: 'R$ 8,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 8
+  },
+  {
+    imagem: 'img/doces/Pirulitos-2d.jpeg',
+    descricao: 'Pirulito 2D',
+    preco: 'R$ 8,50 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 8.50
+  },
+  {
+    imagem: 'img/doces/Pirulitos-3d.jpeg',
+    descricao: 'Pirulito 3D',
+    preco: 'R$ 10,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 10
+  },
+  {
+    imagem: 'img/doces/Cupcakes-2d.jpeg',
+    descricao: 'Cupcake 2D (Personalizado/Com Saiote)',
+    preco: 'R$ 8,50 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 8.50
+  },
+  {
+    imagem: 'img/img-doces-5.jpg',
+    descricao: 'Cupcake 2D (Personalizado/Com Saiote) com recheio',
+    preco: 'R$ 10,00  und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 10
+  },
+  {
+    imagem: 'img/img-doces-6.jpg',
+    descricao: 'Cupcake chantilly',
+    preco: 'R$ 4,50 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 4.50
+  },
+  {
+    imagem: 'img/doces/Cupcakes-chantilly.jpeg',
+    descricao: 'Cupcake chantilly - com recheio',
+    preco: 'R$ 6,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 6
+  },
+  {
+    imagem: 'img/doces/Cupcakes-3d.jpeg',
+    descricao: 'Cupcake 3D',
+    preco: 'R$ 12,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 12
+  },
+  {
+    imagem: 'img/doces/Picolé-2d.jpeg',
+    descricao: 'Picolé 2D',
+    preco: 'R$ 9,50 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 9.50
+  },
+  {
+    imagem: 'img/doces/Picolé-3d.jpeg',
+    descricao: 'Picolé 3D',
+    preco: 'R$ 16,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 16
+  },
+  {
+    imagem: 'img/doces/Trufa-2d.jpeg',
+    descricao: 'Trufa 2D',
+    preco: 'R$ 9,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 9
+  },
+  {
+    imagem: 'img/doces/Trufa-personagem.jpeg',
+    descricao: 'Trufa Personagem',
+    preco: 'R$ 10,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 10
+  },
+  {
+    imagem: 'img/doces/Trufa-3d-personagem.jpeg',
+    descricao: 'Trufa 3D com personagem',
+    preco: 'R$ 15,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 15
+  },
+  {
+    imagem: 'img/doces/Trufa-3d-dupla.jpeg',
+    descricao: 'Trufa 3D dupla',
+    preco: 'R$ 18,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 18
+  },
+  {
+    imagem: 'img/doces/Bolo-palito-2d.jpeg',
+    descricao: 'Bolo no Palito 2D',
+    preco: 'R$ 13,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 13
+  },
+  {
+    imagem: 'img/doces/Cone-personagem.jpeg',
+    descricao: 'Cone com personagem',
+    preco: 'R$ 12,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 12
+  },
+  {
+    imagem: 'img/doces/Bolo-3d.jpeg',
+    descricao: 'Bolo 3D (Casinha / Poça de lama / Cama / Personagens sentados)',
+    preco: 'R$ 20,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 20
+  },
+  {
+    imagem: 'img/doces/Modelado-ninho.jpeg',
+    descricao: 'Doces modelado de leite ninho',
+    preco: 'R$ 7,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 7
+  },
+  {
+    imagem: 'img/doces/Modelado-ninho.jpeg',
+    descricao: 'Modelados leite ninho',
+    preco: 'R$ 250,00 (cento)',
+    und: 'Cento',
+    quant: 1,
+    valor: 250
+  },
+  {
+    imagem: 'img/img-doces-19.jpg',
+    descricao: 'Doces personalizados',
+    preco: 'R$ 220,00 (cento)',
+    und: 'Cento',
+    quant: 1,
+    valor: 220
+  },
+  {
+    imagem: 'img/doces/Trufas-personalizadas( centro).jpeg',
+    descricao: 'Trufas personalizadas',
+    preco: 'R$ 200,00 (cento)',
+    und: 'Cento',
+    quant: 1,
+    valor: 200
+  },
+  {
+  imagem: 'img/doces/casadinho.jpg',
+  descricao: 'Casadinho',
+  preco: 'R$ 130,00 (cento)',
+  und: 'Cento',
+  quant: 1,
+  valor: 130
+  },
+  {
+    imagem: 'img/doces/brigadeiro.jpg',
+    descricao: 'Brigadeiro',
+    preco: 'R$ 120,00 (cento)',
+    und: 'Cento',
+    quant: 1,
+    valor: 120
+  },
+  {
+    imagem: 'img/doces/beijinho.jpg',
+    descricao: 'Beijinho',
+    preco: 'R$ 120,00 (cento)',
+    und: 'Cento',
+    quant: 1,
+    valor: 120
+  },
+  {
+    imagem: 'img/doces/coco-queimado.jpg',
+    descricao: 'Côco queimado',
+    preco: 'R$ 140,00 (cento)',
+    und: 'Cento',
+    quant: 1,
+    valor: 140
+  },
+  {
+    imagem: 'img/doces/surpresa-uva.jpg',
+    descricao: 'Surpresa de uva',
+    preco: 'R$ 150,00 (cento)',
+    und: 'Cento',
+    quant: 1,
+    valor: 150
+  },
+  {
+    imagem: 'img/doces/ninho-nutella.jpg',
+    descricao: 'Ninho com Nutella',
+    preco: 'R$ 150,00 (cento)',
+    und: 'Cento',
+    quant: 1,
+    valor: 150
+  },
+  {
+    imagem: 'img/doces/brigadeiro-gourmet.jpg',
+    descricao: 'Brigadeiro gourmet',
+    preco: 'R$ 150,00 (cento)',
+    und: 'Cento',
+    quant: 1,
+    valor: 150
+  },
+  {
+    imagem: 'img/doces/brigadeiro-branco.jpg',
+    descricao: 'Brigadeiro branco gourmet',
+    preco: 'R$ 150,00 (cento)',
+    und: 'Cento',
+    quant: 1,
+    valor: 150
+  },
+  {
+    imagem: 'img/doces/romeu-julieta.jpg',
+    descricao: 'Romeu e Julieta',
+    preco: 'R$ 130,00 (cento)',
+    und: 'Cento',
+    quant: 1,
+    valor: 130
+  },
+  {
+    imagem: 'img/doces/churros.jpg',
+    descricao: 'Churros',
+    preco: 'R$ 130,00 (cento)',
+    und: 'Cento',
+    quant: 1,
+    valor: 130
+  },
+  {
+    imagem: 'img/doces/morango.jpg',
+    descricao: 'Morangos',
+    preco: 'R$ 180,00 (cento)',
+    und: 'Cento',
+    quant: 1,
+    valor: 180
+  },
+  {
+    imagem: 'img/doces/cajuzinho.jpg',
+    descricao: 'Cajuzinho (Amendoim)',
+    preco: 'R$ 100,00 (cento)',
+    und: 'Cento',
+    quant: 1,
+    valor: 100
+  },
+  {
+    imagem: 'img/doces/olho-de-sogra.jpg',
+    descricao: 'Olho de sogra',
+    preco: 'R$ 140,00 (cento)',
+    und: 'Cento',
+    quant: 1,
+    valor: 140
+  },
+  {
+    imagem: 'img/doces/trufas-diversas.jpg',
+    descricao: 'Trufas (Diversos sabores)',
+    preco: 'R$ 230,00 (cento)',
+    und: 'Cento',
+    quant: 1,
+    valor: 230
+  },
+  {
+    imagem: 'img/doces/copo-chocolate.jpg',
+    descricao: 'Copo de chocolate',
+    preco: 'R$ 130,00 (cento)',
+    und: 'Cento',
+    quant: 1,
+    valor: 130
+  },
+  {
+    imagem: 'img/doces/copo-chocolate-cereja.jpg',
+    descricao: 'Copo de chocolate com cereja',
+    preco: 'R$ 160,00 (cento)',
+    und: 'Cento',
+    quant: 1,
+    valor: 160
+  },
+  {
+    imagem: 'img/doces/bem-casado-crepom.jpg',
+    descricao: 'Bem casado (crepom)',
+    preco: 'R$ 7,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 7
+  },
+  {
+    imagem: 'img/doces/bem-casado-tecido.jpg',
+    descricao: 'Bem casado (tecido)',
+    preco: 'R$ 9,00 und',
+    und: 'Unidade',
+    quant: 1,
+    valor: 9
+  }
+]
+};
+
+const RECHEIOS_GRATIS = [
+  "Ameixa",
+  "Abacaxi",
+  "Amendoim",
+  "Brigadeiro",
+  "Coco",
+  "Côco queimado",
+  "Doce de leite",
+  "Maracuja",
+  "Ninho"
+];
+
+const RECHEIOS_PAGOS = [
+  { nome: "Morango", valor: 15 },
+  { nome: "Castanha do pará", valor: 15 },
+  { nome: "Nozes", valor: 15 },
+  { nome: "Damasco", valor: 15 }
+];
